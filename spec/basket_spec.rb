3fb98ff824f1cf43e99d5a6591f7f4c633b005dd
David Wickes <dave.wickes@gmail.com>
2014-08-19 11:52:55 +0100

    new test for basket accepting products

--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,11 +1,15 @@
 require 'basket'
 
 describe Basket do
+	let(:basket) { Basket.new }
 
 	it 'should initialized empty' do
-		basket = Basket.new
-
 		expect(basket.has_items?).to be false
 	end	
+
+	it 'receives products' do
+		product = double :product
+		expect(basket.receive(product)).to eq basket
+	end
 
 end
