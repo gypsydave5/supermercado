ea54d76eda991ea0b06dc57ede63f3ac7c9f13f3
David Wickes <dave.wickes@gmail.com>
2014-08-19 17:01:49 +0100

    added test to see if the till received the dumped basket items

--- lib/till.rb
+++ lib/till.rb
@@ -1,8 +1,12 @@
 class Till
+	def initialize
+		@basket = nil
+	end
 
 	def accept_basket basket
-
-		basket
+		raise ArgumentError, 'No items in basket!' unless basket.has_items?
+		@basket = basket
+		self
 	end
 
 
--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -2,16 +2,29 @@ require 'till'
 
 describe Till do
 		
-	let(:basket)  {double :basket}
-	let(:till)    {Till.new      }
+	let ( :basket )  { double :basket, has_items?: true, dump_all!: { product_one: 1, product_two: 2 } }
+	let ( :product_one ) { double :product, id: :baked_beans }
+	let ( :product_two ) { double :product, id: :cigarettes }
+	let (:price_list ) { double :price_list  }
+	let ( :till   )  { Till.new                         } 
 
 	it 'accept a basket of products' do
-		expect(till.accept_basket(basket)).to be basket
+		expect(till.accept_basket(basket)).to be till
 	end
 
 	it 'raise error if basket is empty' do
 		basket_empty = double :basket, has_items?: false
+		expect{till.accept_basket(basket_empty)}.
+			to raise_error(ArgumentError, "No items in basket!")
+	end
 
-		expect{till.accept_basket(basket_empty)}.to raise_error(ArgumentError, "No items in basket!")
+	it 'received the items dumped by the basket' do
+		expect(till.accept_basket(basket).items_in_till).
+			to be ({ product_one: 1, product_two: 2 })
+	end
+
+	xit 'calculates the total value of the items in the basket' do
+		allow(price_list).to receive(:price_of).with(:cigarettes).and_return(7.99)
+		allow(price_list).to receive(:price_of).with(:baked_beans).and_return(0.59)
 	end
 end
